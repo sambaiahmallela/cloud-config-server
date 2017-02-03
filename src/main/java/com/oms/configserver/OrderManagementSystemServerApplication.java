303ce04f16b8c7392ba80d0f77082fe14149ed8f
“sambaiahmallela” <sambaiah.mallela@gmail.com>
2017-02-03 12:01:21 +0530

    initial commit

--- src/main/java/com/oms/configserver/OrderManagementSystemServerApplication.java
+++ src/main/java/com/oms/configserver/OrderManagementSystemServerApplication.java
@@ -1,14 +1,10 @@
 package com.oms.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableConfigServer
 public class OrderManagementSystemServerApplication {
 
